2f1d34f33ec5b7f639f69561e80cbf54b75897c7
yongwoo jeon <mixed.jeon@gmail.com>
2016-10-18 19:41:12 +0900

    test

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById('test3').addEventListener("click", function() {
+	document.getElementById('test4').addEventListener("click", function() {
 		alert("click");
 	});	
 });
