e694cdf27b7113ec403d08becbb708ccc23deec7
yongwoo jeon <mixed.jeon@gmail.com>
2016-10-18 18:11:36 +0900

    revert es6

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 
-document.addEventListener("DOMContentLoaded",function _ => {
-	document.getElementById('test').addEventListener("click", _ => {
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById('test').addEventListener("click", function() {
 		alert("click");
 	});	
 });
