7dde047a2a5cfb20a0465b6effc227abd65c57b7
전용우 <mixed.jeon@gmail.com>
2016-10-18 19:38:59 +0900

    다시 수정 (#6)

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById('test').addEventListener("click", function() {
+	document.getElementById('test3').addEventListener("click", function() {
 		alert("click");
 	});	
 });
