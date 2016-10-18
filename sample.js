29631a83e8cb543e1203a21d134045fb53444cc6
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2016-10-18 21:03:41 +0900

    아이디 변경 9

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById('test3').addEventListener("click", function() {
+	document.getElementById('test9').addEventListener("click", function() {
 		alert("click");
 	});	
 });
