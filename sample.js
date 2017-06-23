6cf1394fa6b24722ba97b792d91dfedbb7650cf9
yongwoo <mixed.jeon@gmail.com>
2017-06-23 15:30:33 +0900

    test2

--- sample.js
+++ sample.js
@@ -1,7 +1,4 @@
-
-dd
-d
-iocument.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById('test10').addEventListener("click", function() {
 });
 
