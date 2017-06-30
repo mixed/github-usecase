52b54ca5eabd71e9d566fd22e5f47ae581e27dc1
전용우 <mixed.jeon@gmail.com>
2017-06-30 18:47:44 +0900

    first (#32)

--- sample.js
+++ sample.js
@@ -1,29 +1,4 @@
-
-
-
-
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById('test10').addEventListener("click", function() {
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-o
-
-
-
 
